c02c619b9d6e0019815621d172ba073e8c2027e9
Rolex-444 <sivaraman.gc49@gmail.com>
2025-10-21 11:33:48 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -1,42 +1,35 @@
-from pyrogram import Client, filters
+from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from database.users import UserDB
 from config import Config
-
-user_db = UserDB()
 
 @Client.on_callback_query(filters.regex("^help$"))
 async def help_callback(client, query):
     buttons = [
-        [InlineKeyboardButton("🔍 How to Search", callback_data="search_help")],
-        [InlineKeyboardButton("🔐 Verification", callback_data="verify_help")],
-        [InlineKeyboardButton("👑 Premium", callback_data="premium")],
-        [InlineKeyboardButton("🔞 18+ Rare Videos", url="https://t.me/REAL_TERABOX_PRO_bot")],
-        [InlineKeyboardButton("🏠 Home", callback_data="start")]
-    ]
-    await query.message.edit(
-        Config.HELP_TXT,
-        reply_markup=InlineKeyboardMarkup(buttons),
-        parse_mode="html",
-        disable_web_page_preview=True
-    )
-
-@Client.on_callback_query(filters.regex("^about$"))
-async def about_callback(client, query):
-    buttons = [
-        [InlineKeyboardButton("🔞 18+ Rare Videos", url="https://t.me/REAL_TERABOX_PRO_bot")],
         [InlineKeyboardButton("🏠 Home", callback_data="start")],
         [InlineKeyboardButton("❌ Close", callback_data="close")]
     ]
     await query.message.edit(
-        Config.ABOUT_TXT.format(client.username),
+        Config.HELP_TXT,
         reply_markup=InlineKeyboardMarkup(buttons),
-        parse_mode="html",
-        disable_web_page_preview=True
+        parse_mode=enums.ParseMode.HTML
+    )
+
+@Client.on_callback_query(filters.regex("^about$"))
+async def about_callback(client, query):
+    me = await client.get_me()
+    buttons = [
+        [InlineKeyboardButton("🏠 Home", callback_data="start")],
+        [InlineKeyboardButton("❌ Close", callback_data="close")]
+    ]
+    await query.message.edit(
+        Config.ABOUT_TXT.format(me.username),
+        reply_markup=InlineKeyboardMarkup(buttons),
+        parse_mode=enums.ParseMode.HTML
     )
 
 @Client.on_callback_query(filters.regex("^start$"))
 async def start_callback(client, query):
+    first_name = query.from_user.first_name
     buttons = [
         [InlineKeyboardButton("🆘 Help", callback_data="help"),
          InlineKeyboardButton("ℹ️ About", callback_data="about")],
@@ -46,9 +39,12 @@ async def start_callback(client, query):
         [InlineKeyboardButton("🎬 Join Channel", url="https://t.me/movies_magic_club3")]
     ]
     await query.message.edit(
-        Config.START_TXT.format(query.from_user.first_name),
+        Config.START_TXT.format(first_name),
         reply_markup=InlineKeyboardMarkup(buttons),
-        parse_mode="html",
-        disable_web_page_preview=True
-            )
+        parse_mode=enums.ParseMode.HTML
+    )
+
+@Client.on_callback_query(filters.regex("^close$"))
+async def close_callback(client, query):
+    await query.message.delete()
     
